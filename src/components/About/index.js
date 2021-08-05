f3b7e64121bf4c192f6115a1693b3c12f3a444f3
Lex Crawford <lexcraw4d@gmail.com>
2021-08-05 16:21:55 -0400

    added props for categories in Nav component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import coverImage from '../../Assets/cover/cover-image.jpg'
 
 function About() {
     return (
-        <section className= "my-5">
+        <section className= "m-5">
             <h1 id="about"> Who am I?</h1>
             <img src = {coverImage} className= "my-2" style = {{ width: "100%"}} alt = "cover" />
         </section>
